2cb099653ca9f6b13fdb0ee2d17ed55e09172147
shabnamzar <73387454+shabnamzar@users.noreply.github.com>
2020-10-25 08:51:06 +0330

    Add files via upload

---
+++ assets/js/pages/service.js
@@ -0,0 +1,40 @@
+﻿var playPause = document.getElementById("btnvideo");
+var playPause_i = document.querySelector("#btnvideo i");
+var vid = document.querySelector(".videoBox video");
+if (typeof playPause !== "undefined") {
+    playPause.addEventListener(
+        "click",
+        function (e) {
+            var t1 = document.querySelector("#btnvideo");
+            var media = document.querySelector(".media");
+            e.preventDefault();
+            if (!vid.paused) {
+                playPause.classList.add("colored");
+                playPause_i.classList.add("fa-play-circle");
+                playPause_i.classList.remove("fa-pause-circle");
+                vid.removeAttribute("controls");
+                vid.pause();
+            } else {
+                playPause.classList.remove("colored");
+                playPause_i.classList.remove("fa-play-circle");
+                playPause_i.classList.add("fa-pause-circle");
+                vid.setAttribute("controls", "");
+                vid.play();
+            }
+            return false;
+        },
+        true
+    );
+}
+function setWidthVideo() {
+    var width = $("video").css("width");
+    if (typeof width !== "undefined") {
+        $("#btnvideo").css("width", width);
+    }
+}
+$(document).ready(function () {
+    setWidthVideo();
+});
+$(window).resize(function () {
+    setWidthVideo();
+});
